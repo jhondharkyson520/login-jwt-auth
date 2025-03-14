001e868951bfcd75c392f86783024254af80bbf2
Jhon <jhondharkyson@gmail.com>
2025-03-14 15:56:00 -0300

    Feat: add middleware for route authorization, or private routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { createUserController } from "../controllers/create-user-controller";
 import { authenticateUserController } from "../controllers/authenticate-user-controller";
+import { authenticateToken } from "../middleware/authorization-routes";
 
 const userRoutes = Router();
 userRoutes.post('/create', createUserController);
 userRoutes.post('/login', authenticateUserController);
+userRoutes.get('/hello', authenticateToken, (req: Request, res: Response) => {
+    return res.status(200).json({message: 'Hello World'})
+})
 
 export default userRoutes;
